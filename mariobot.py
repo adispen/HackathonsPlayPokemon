16ce25ea76eead421cda0cc4d6272786a2a33a29
Aedan Dispenza <aedan.dispenza@gmail.com>
2014-02-23 16:47:55 -0500

    added mariobot to pipe pokemon commands into mario

---
+++ mariobot.py
@@ -0,0 +1,326 @@
+# Import some necessary libraries.
+import socket 
+import ctypes
+import time
+import random
+import re
+import win32api
+import win32con
+import random
+VK_CODE = {'backspace':0x08,
+           'tab':0x09,
+           'clear':0x0C,
+           'enter':0x0D,
+           'shift':0x10,
+           'ctrl':0x11,
+           'alt':0x12,
+           'pause':0x13,
+           'caps_lock':0x14,
+           'esc':0x1B,
+           'spacebar':0x20,
+           'page_up':0x21,
+           'page_down':0x22,
+           'end':0x23,
+           'home':0x24,
+           'left_arrow':0x25,
+           'up_arrow':0x26,
+           'right_arrow':0x27,
+           'down_arrow':0x28,
+           'select':0x29,
+           'print':0x2A,
+           'execute':0x2B,
+           'print_screen':0x2C,
+           'ins':0x2D,
+           'del':0x2E,
+           'help':0x2F,
+           '0':0x30,
+           '1':0x31,
+           '2':0x32,
+           '3':0x33,
+           '4':0x34,
+           '5':0x35,
+           '6':0x36,
+           '7':0x37,
+           '8':0x38,
+           '9':0x39,
+           'a':0x41,
+           'b':0x42,
+           'c':0x43,
+           'd':0x44,
+           'e':0x45,
+           'f':0x46,
+           'g':0x47,
+           'h':0x48,
+           'i':0x49,
+           'j':0x4A,
+           'k':0x4B,
+           'l':0x4C,
+           'm':0x4D,
+           'n':0x4E,
+           'o':0x4F,
+           'p':0x50,
+           'q':0x51,
+           'r':0x52,
+           's':0x53,
+           't':0x54,
+           'u':0x55,
+           'v':0x56,
+           'w':0x57,
+           'x':0x58,
+           'y':0x59,
+           'z':0x5A,
+           'numpad_0':0x60,
+           'numpad_1':0x61,
+           'numpad_2':0x62,
+           'numpad_3':0x63,
+           'numpad_4':0x64,
+           'numpad_5':0x65,
+           'numpad_6':0x66,
+           'numpad_7':0x67,
+           'numpad_8':0x68,
+           'numpad_9':0x69,
+           'multiply_key':0x6A,
+           'add_key':0x6B,
+           'separator_key':0x6C,
+           'subtract_key':0x6D,
+           'decimal_key':0x6E,
+           'divide_key':0x6F,
+           'F1':0x70,
+           'F2':0x71,
+           'F3':0x72,
+           'F4':0x73,
+           'F5':0x74,
+           'F6':0x75,
+           'F7':0x76,
+           'F8':0x77,
+           'F9':0x78,
+           'F10':0x79,
+           'F11':0x7A,
+           'F12':0x7B,
+           'F13':0x7C,
+           'F14':0x7D,
+           'F15':0x7E,
+           'F16':0x7F,
+           'F17':0x80,
+           'F18':0x81,
+           'F19':0x82,
+           'F20':0x83,
+           'F21':0x84,
+           'F22':0x85,
+           'F23':0x86,
+           'F24':0x87,
+           'num_lock':0x90,
+           'scroll_lock':0x91,
+           'left_shift':0xA0,
+           'right_shift ':0xA1,
+           'left_control':0xA2,
+           'right_control':0xA3,
+           'left_menu':0xA4,
+           'right_menu':0xA5,
+           'browser_back':0xA6,
+           'browser_forward':0xA7,
+           'browser_refresh':0xA8,
+           'browser_stop':0xA9,
+           'browser_search':0xAA,
+           'browser_favorites':0xAB,
+           'browser_start_and_home':0xAC,
+           'volume_mute':0xAD,
+           'volume_Down':0xAE,
+           'volume_up':0xAF,
+           'next_track':0xB0,
+           'previous_track':0xB1,
+           'stop_media':0xB2,
+           'play/pause_media':0xB3,
+           'start_mail':0xB4,
+           'select_media':0xB5,
+           'start_application_1':0xB6,
+           'start_application_2':0xB7,
+           'attn_key':0xF6,
+           'crsel_key':0xF7,
+           'exsel_key':0xF8,
+           'play_key':0xFA,
+           'zoom_key':0xFB,
+           'clear_key':0xFE,
+           '+':0xBB,
+           ',':0xBC,
+           '-':0xBD,
+           '.':0xBE,
+           '/':0xBF,
+           '`':0xC0,
+           ';':0xBA,
+           '[':0xDB,
+           '\\':0xDC,
+           ']':0xDD,
+           "'":0xDE,
+           '`':0xC0}
+
+def press(*args):
+    '''
+    one press, one release.
+    accepts as many arguments as you want. e.g. press('left_arrow', 'a','b').
+    '''
+    for i in args:
+        win32api.keybd_event(VK_CODE[i], 0,0,0)
+        time.sleep(.1)
+        win32api.keybd_event(VK_CODE[i],0 ,win32con.KEYEVENTF_KEYUP ,0)
+
+
+# Some basic variables used to configure the bot        
+#server = "irc.twitch.tv" # Server
+#channel = "#HackathonsPlayPokemon" # Channel
+botnick = "HackathonsPlayPokemon" # Your bots nick
+#password = "oauth:878t7gk5f9z1ez6xtxxaa9hdhoq41d7"
+#port = "6667"
+
+HOST="irc.twitch.tv" ##This is the Twitch IRC ip, don't change it.
+PORT=6667 ##Same with this port, leave it be.
+NICK="HackathonsPlayPokemon" ##This has to be your bots username.
+PASS="" ##Instead of a password, use this http://twitchapps.com/tmi/, since Twitch is soon updating to it.
+IDENT="HackathonsPlayPokemon" ##Bot username again
+REALNAME="whoCares" ##This doesn't really matter.
+CHANNEL="#twitchplayspokemon" ##This is the channel your bot will be working on.
+
+
+def ping(): # This is our first function! It will respond to server Pings.
+  s.send("PONG :pingis\n")  
+
+def sendmsg(chan , msg): # This is the send message function, it simply sends messages to the channel.
+  s.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
+
+def joinchan(chan): # This function is used to join channels.
+  s.send("JOIN "+ chan +"\n")
+
+def authenticate(server, port, auth):
+  s.send("SERVER " + server + " " + port + " " + auth)
+
+def hello(): # This function responds to a user that inputs "Hello Mybot"
+  s.send("PRIVMSG "+ channel +" :Hello!\n")
+
+def aPress():
+
+  #PressKey(0x5A) # A key which is Z for emulator
+  #ReleaseKey(0x5A)
+  press('z')
+
+def bPress():
+  
+  #PressKey(0x58) # B key which is X for emulator
+  #ReleaseKey(0x58)
+  press(random.choice(['a', 'd']))
+  press('z')
+
+def upPress():
+
+  #PressKey(0x26) # Up on DPad which is Up Arrow for emulator
+  #ReleaseKey(0x26)
+  press('d')
+
+def downPress():
+
+  #PressKey(0x28) # Down on DPad which is Down Arrow f7or emulator
+  #ReleaseKey(0x28)
+  press('s')
+
+def leftPress():
+
+  #PressKey(0x25) # Left on DPad which is Left Arrow for emulator
+  #ReleaseKey(0x25)
+  press('a')
+
+def rightPress():
+
+  #PressKey(0x27) # Right on DPad which is Right Arrow for emulator
+  #ReleaseKey(0x27) 
+  press('d')
+
+def startPress():
+  
+  #PressKey(0x56) # Start key which is V for emulator
+  #time.sleep(2)
+  #ReleaseKey(0x56)
+  press('x')
+
+#import pdb; pdb.set_trace()
+s = socket.socket( ) ##Creating the socket variable
+s.connect((HOST, PORT)) ##Connecting to Twitch
+s.send("PASS %s\r\n" % PASS) ##Notice how I'm sending the password BEFORE the username!
+##Just sending the rest of the data now.
+s.send("NICK %s\r\n" % NICK)
+s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+##Connecting to the channel.
+s.send("JOIN %s\r\n" % CHANNEL)
+
+
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#ircsock.connect((HOST, 6667)) # Here we connect to the server using the port 6667
+#ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This bot is a result of a tutoral covered on http://shellium.org/wiki.\n") # user authentication
+#ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+
+#authenticate(server, port, password)
+#joinchan(channel) # Join the channel using the functions we previously defined
+
+IRC = "#HackathonsPlayPokemon "
+
+def printCommands(name, command):
+  spaces = ""
+  spaceBetween = 25 - (len(name)+len(command))
+  while spaceBetween >0:
+    spaces += " "
+    spaceBetween -= 1
+  print name+spaces+command
+
+
+
+while 1: # Be careful with these! it might send you to an infinite loop
+  ircmsg = s.recv(2048) # receive data from the server
+  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
+  #print(ircmsg) # Here we print what's coming from the server
+
+  if ircmsg.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
+      hello()
+
+  if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
+      ping()
+
+
+  input = ircmsg
+  expression = ':(.+)!.+ PRIVMSG #(.+) :(.+)'
+  output = re.search(expression, input)
+
+  if output:
+    #import pdb; pdb.set_trace()
+    name = output.group(1)
+    command = output.group(3).strip().lower()
+
+
+    if command == "a": # Finding messages
+      aPress()
+      printCommands(name, command)
+
+    if command == "b": # Finding messages
+      bPress()
+      printCommands(name, command)
+
+    if command == "up": # Finding messages
+      upPress()
+      printCommands(name, command)
+
+    if command == "down": # Finding messages
+      rightPress()
+      printCommands(name, command)
+
+    if command == "left": # Finding messages
+      leftPress()
+      leftPress()
+      printCommands(name, command)
+
+    if command == "right": # Finding messages
+      rightPress()
+      rightPress()
+      printCommands(name, command)
+
+    if command == "start": # Finding messages
+      startPress()
+      printCommands(name, command)
+
+# Keyboard Commands
