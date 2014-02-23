8ba9e89167443afb5b505ef6ca5ed0cfe0eed332
Sam Agnew <sagnew92@gmail.com>
2014-02-23 00:59:26 -0500

    Changed bot name

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import re
 # Some basic variables used to configure the bot        
 server = "irc.freenode.net" # Server
 channel = "#jacked" # Channel
-botnick = "PokemonTest%d" % random.randint(1,10000) # Your bots nick
+botnick = "HackathonsPlayPokemon" # Your bots nick
 
 
 def ping(): # This is our first function! It will respond to server Pings.
